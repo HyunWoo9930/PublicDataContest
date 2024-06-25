242d86765c6c3cd2f31c818e435938f9b3b9537b
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-06-25 18:37:37 +0900

    [ADD] First Commit

---
+++ src/main/java/org/example/publicdatacontest/PublicDataContestApplication.java
@@ -0,0 +1,13 @@
+package org.example.publicdatacontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDataContestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicDataContestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/publicdatacontest/PublicDataContestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.publicdatacontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicDataContestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
